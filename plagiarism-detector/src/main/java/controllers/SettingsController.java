42c124ee5a87f5fc3acf8591f1edc31b1f56c250
Samanjate Sood <samanjate@ccs.neu.edu>
2018-03-04 17:45:57 -0500

    integrated front end

---
+++ plagiarism-detector/src/main/java/controllers/SettingsController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+/**
+ * This is a Controller for the Setting page of the application.
+ * 
+ * Note: This class will be implemented in the next sprint.
+ * 
+ * @author Samanjate Sood
+ *
+ */
+public class SettingsController {
+
+}
---
+++ plagiarism-detector/src/main/java/view/Main.java
@@ -0,0 +1,34 @@
+package view;
+	
+import java.util.logging.Logger;
+
+import controllers.ScreenController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	
+	private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+	private String errorMessage = "An Error Occured!";
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/Start.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			ScreenController.setInstance(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			LOGGER.info(errorMessage);
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
