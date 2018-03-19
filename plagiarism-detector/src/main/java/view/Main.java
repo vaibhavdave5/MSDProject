66b28f3064b897c512a1081c7c8fa84dcf7a9f8f
Samanjate Sood <samanjate@ccs.neu.edu>
2018-03-19 18:28:22 -0400

    deleted ui

--- plagiarism-detector/src/main/java/view/Main.java
+++ plagiarism-detector/src/main/java/view/Main.java
@@ -1,11 +1,6 @@
 package view;
 	
-import java.util.logging.Logger;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 /**
  * Entry point of application  
@@ -16,20 +11,9 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	
-	private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
-	private String errorMessage = "An Error Occured!";
-	
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/Start.fxml"));
-			Scene scene = new Scene(root);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-			primaryStage.close();
-		} catch(Exception e) {
-			LOGGER.info(errorMessage);
-		}
+		// Empty method
 	}
 	
 	public static void main(String[] args) {
