612aab7e19968d58d673f639f9209931983e1037
Samanjate Sood <samanjate@ccs.neu.edu>
2018-03-20 19:05:11 -0400

    base front page

--- plagiarism-detector/src/main/java/controllers/MainController.java
+++ plagiarism-detector/src/main/java/controllers/MainController.java
@@ -1,11 +1,51 @@
 package controllers;
 
+import java.io.File;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+
 /**
  * This Controller is responsible to load the main page of the application.
  * 
  * @author Samanjate Sood
+ * @author Shail Shah
  *
  */
 public class MainController {
-	//Empty class
+	
+	@FXML private Label dirName;
+	@FXML private ListView<String> dirContent;
+	@FXML private TextField filterVal;
+	
+	public void browseDirectory() {
+		try {
+			File directory = selectDirectory();
+			setDirectoryLabel(directory);
+			populateView(directory);
+		} catch(Exception e) { }
+	}
+	
+	private File selectDirectory() {
+		return new DirectoryChooser().showDialog(null);
+	}
+	
+	private void setDirectoryLabel(File directory) {
+		if(directory == null) {
+			dirName.setText("No Directory selected");
+		} else {
+			dirName.setText(directory.getAbsolutePath());
+		}
+	}
+	
+	private void populateView(File directory) {
+		dirContent.getItems().clear();
+		for(File file : directory.listFiles()) {
+			dirContent.getItems().add(file.getName());
+		}
+	}
 }
--- plagiarism-detector/src/main/java/view/Main.java
+++ plagiarism-detector/src/main/java/view/Main.java
@@ -1,10 +1,14 @@
 package view;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 /**
  * Entry point of application  
- * @author Samanjate
+ * @author Samanjate Sood
+ * @author Shail Shah
  * @since 01-03-2018
  *
  */ 
@@ -13,9 +17,17 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
-		// Empty method
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/Start.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setResizable(false);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
