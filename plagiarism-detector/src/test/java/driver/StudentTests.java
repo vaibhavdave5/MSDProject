4a08c0c8c625135c38474d70378c19d9fd3925d3
darshanpanse <panse.d@husky.neu.edu>
2018-03-20 16:43:15 -0400

    CS107-7-extended: Added student tests

--- plagiarism-detector/src/test/java/driver/StudentTests.java
+++ plagiarism-detector/src/test/java/driver/StudentTests.java
@@ -7,8 +7,23 @@ import org.junit.Test;
 public class StudentTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetters() {
+		Student s = new Student(101, "John", "s.s@husky.neu.edu");
+		assertEquals(101, s.getId());
+		assertEquals("John", s.getName());
+		assertEquals("s.s@husky.neu.edu", s.getEmail());
 	}
-
+	
+	@Test
+	public void testSetters() {
+		Student s = new Student(101, "Sam", "s.s@husky.neu.edu");
+		s.setId(102);
+		assertEquals(102, s.getId());
+		
+		s.setName("John");
+		assertEquals("John", s.getName());
+		
+		s.setEmail("j.j@husky.neu.edu");
+		assertEquals("j.j@husky.neu.edu", s.getEmail());
+	}
 }
