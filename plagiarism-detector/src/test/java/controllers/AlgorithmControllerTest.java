01139e593d1ad2cde956c18de22613e0530aaf02
darshanpanse <panse.d@husky.neu.edu>
2018-03-20 15:52:30 -0400

    CS107-7-extended: moved Student tests to Algorithm Controller for testing purposes

--- plagiarism-detector/src/test/java/controllers/AlgorithmControllerTest.java
+++ plagiarism-detector/src/test/java/controllers/AlgorithmControllerTest.java
@@ -6,6 +6,7 @@ import java.util.logging.Logger;
 import org.junit.Test;
 import algorithms.LCSAlgorithm;
 import algorithms.NeemanWalshAlgorithm;
+import driver.Student;
 
 /**
  * Tests AlgorithmController to see if the similarity is computed as expected.
@@ -47,6 +48,27 @@ public class AlgorithmControllerTest {
 		double ans = -1;
 		ans = ac.getAns(new NeemanWalshAlgorithm());
 		assertTrue(ans >= 0 && ans <= 1);
+	}
+	
+	@Test
+	public void testGetters() {
+		Student s = new Student(101, "Sam", "s.s@husky.neu.edu");
+		assertEquals(101, s.getId());
+		assertEquals("Sam", s.getName());
+		assertEquals("s.s@husky.neu.edu", s.getEmail());
+	}
+	
+	@Test
+	public void testSetters() {
+		Student s = new Student(101, "Sam", "s.s@husky.neu.edu");
+		s.setId(102);
+		assertEquals(102, s.getId());
+		
+		s.setName("John");
+		assertEquals("John", s.getName());
+		
+		s.setEmail("j.j@husky.neu.edu");
+		assertEquals("j.j@husky.neu.edu", s.getEmail());
 	}
 
 	/**
