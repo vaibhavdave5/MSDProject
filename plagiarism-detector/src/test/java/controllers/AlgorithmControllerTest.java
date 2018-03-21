8592a9889b654cdc5bf6aef43ad7bf9fb4fd98d4
darshanpanse <panse.d@husky.neu.edu>
2018-03-20 22:14:17 -0400

    CS107-7 added the commented out tests back

--- plagiarism-detector/src/test/java/controllers/AlgorithmControllerTest.java
+++ plagiarism-detector/src/test/java/controllers/AlgorithmControllerTest.java
@@ -28,36 +28,36 @@ public class AlgorithmControllerTest {
 	 * Tests the simple small files
 	 */
 
-//	@Test
-//	public void test() {
-//		String path1 = "sample3.c";
-//		String path2 = "sample4.c";
-//		File file1 = new File(path1);
-//		File file2 = new File(path2);
-//
-//		AlgorithmController ac = new AlgorithmController(file1, file2);
-//		double ans = -1;
-//		ans = ac.getAns(new LCSAlgorithm());
-//		System.out.println(ans);
-//		assertTrue(ans >= 0 && ans <= 1);
-//	}
-//
-//	/**
-//	 * Tests larger files
-//	 */
-//	@Test
-//	public void test2() {
-//		String path1 = "sample3.c";
-//		String path2 = "sample4.c";
-//		File file1 = new File(path1);
-//		File file2 = new File(path2);
-//
-//		AlgorithmController ac = new AlgorithmController(file1, file2);
-//		double ans = -1;
-//		ans = ac.getAns(new NeemanWalshAlgorithm());
-//		System.out.println(ans);
-//		assertTrue(ans >= 0 && ans <= 1);
-//	}
+	@Test
+	public void test() {
+		String path1 = "sample3.c";
+		String path2 = "sample4.c";
+		File file1 = new File(path1);
+		File file2 = new File(path2);
+
+		AlgorithmController ac = new AlgorithmController(file1, file2);
+		double ans = -1;
+		ans = ac.getAns(new LCSAlgorithm());
+		System.out.println(ans);
+		assertTrue(ans >= 0 && ans <= 1);
+	}
+
+	/**
+	 * Tests larger files
+	 */
+	@Test
+	public void test2() {
+		String path1 = "sample3.c";
+		String path2 = "sample4.c";
+		File file1 = new File(path1);
+		File file2 = new File(path2);
+
+		AlgorithmController ac = new AlgorithmController(file1, file2);
+		double ans = -1;
+		ans = ac.getAns(new NeemanWalshAlgorithm());
+		System.out.println(ans);
+		assertTrue(ans >= 0 && ans <= 1);
+	}
 	
 	// Student Tests
 	/////////////////////////////////////////////////////////////////////////////
