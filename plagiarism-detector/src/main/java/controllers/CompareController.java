13808fa48132cefe0fc6e2a9f022f005c9b5275e
Shail Shah <shail@ccs.neu.edu>
2018-03-22 17:51:31 -0400

    CS107-41: Initializes CompareController

--- plagiarism-detector/src/main/java/controllers/CompareController.java
+++ plagiarism-detector/src/main/java/controllers/CompareController.java
@@ -1,4 +1,28 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.text.TextFlow;
+
+/**
+ * Controller for the UI that shows comparision between two submissions
+ * @author Shail Shah
+ */
 public class CompareController {
+	// Controller injectors
+	@FXML private TextFlow studentACode;
+	@FXML private TextFlow studentBCode;
+	@FXML private Button reportButton;
+	@FXML private Label StudentAName;
+	@FXML private Label StudentBName;
+
+	/**
+	 * This method runs on page load and initializes all components of the Start.fxml page
+	 */
+	@FXML
+	protected void initialize() {
+
+	}
+
+
 }
