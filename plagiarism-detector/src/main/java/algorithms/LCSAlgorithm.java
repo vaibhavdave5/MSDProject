6ad6af873d6079ddee8b8f4f70e50f6b22617ff4
Vaibhav Shekhar Dave <vaibhavdave5@ccs.neu.edu>
2018-03-22 13:04:46 -0400

    CS107-7 Refactored LCS and removed bugs

--- plagiarism-detector/src/main/java/algorithms/LCSAlgorithm.java
+++ plagiarism-detector/src/main/java/algorithms/LCSAlgorithm.java
@@ -38,13 +38,14 @@ public class LCSAlgorithm implements AlgorithmStrategy {
 	public Node[] getCommonNodes(List<Node> list1, List<Node> list2) {
 		int[][] map = getMap(list1, list2);
 
-		int i = list1.size()-1;
-		int j = list2.size()-1;
+		int i = list1.size();
+		int j = list2.size();
 		int index = map[i][j];
 		Node[] lcs = new Node[index + 1];
 		lcs[index] = null; 						// Set the terminating character
 		while (i > 0 && j > 0) {
-			if (list1.get(i - 1).equals(list2.get(i - 1))) {
+			
+			if (list1.get(i - 1).equals(list2.get(j - 1))) {
 				lcs[index - 1] = list1.get(i - 1);
 				i--;
 				j--;
