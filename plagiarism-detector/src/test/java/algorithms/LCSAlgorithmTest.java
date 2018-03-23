cdfc1f64a8d05da16bfad2e066ed0cbcf5272750
Vaibhav Shekhar Dave <vaibhavdave5@ccs.neu.edu>
2018-03-23 08:51:53 -0400

    CS107-41 Added TestSuite 3

---
+++ plagiarism-detector/src/test/java/algorithms/LCSAlgorithmTest.java
@@ -0,0 +1,14 @@
+package algorithms;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import utils.FileUtils;
+
+@RunWith(Suite.class)
+@SuiteClasses({ LCSUnitTest.class, NeemanWalshAlgorithmTest.class, NodeTests.class, FileUtils.class })
+
+public class LCSAlgorithmTest {
+	
+}
