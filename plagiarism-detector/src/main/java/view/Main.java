1784d9518494e03378fc295eeeb2e74f132d6e00
Shail Shah <shail@ccs.neu.edu>
2018-03-22 22:34:26 -0400

    CS107-41: Housekeeping

--- plagiarism-detector/src/main/java/view/Main.java
+++ plagiarism-detector/src/main/java/view/Main.java
@@ -23,7 +23,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/Compare.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/Start.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("/theme.css").toExternalForm());
 			primaryStage.setResizable(false);
