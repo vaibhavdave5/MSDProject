919a0d3d6438825aa207b90e56d96d1aef486f17
darshanpanse <panse.d@husky.neu.edu>
2018-03-27 18:52:04 -0400

    CS107-62: Correction in AllTests test suite

--- plagiarism-detector/src/test/java/algorithms/AllTests.java
+++ plagiarism-detector/src/test/java/algorithms/AllTests.java
@@ -4,14 +4,14 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import driver.Driver;
+import driver.DriverTests;
 import driver.StudentPairTests;
 import driver.StudentTests;
 import driver.SummaryTests;
 import utils.FileUtilsTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ StudentPairTests.class, SummaryTests.class, Driver.class, StudentTests.class, LCSUnitTest.class, NeemanWalshAlgorithmTest.class, NodeTests.class, FileUtilsTest.class })
+@SuiteClasses({ StudentPairTests.class, SummaryTests.class, DriverTests.class, StudentTests.class, LCSUnitTest.class, NeemanWalshAlgorithmTest.class, NodeTests.class, FileUtilsTest.class })
 
 public class AllTests {
 
