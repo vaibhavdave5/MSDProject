c71b713e91541ab593c1dd2cd22a2948f7462921
Shail Shah <shail@ccs.neu.edu>
2018-03-27 16:00:45 -0400

    CS107-46: Adds Tests for checking valid emails and sending mails

---
+++ plagiarism-detector/src/test/java/utils/MailUtilsTest.java
@@ -0,0 +1,87 @@
+package utils;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests for mail utilities
+ * @author Shail Shah
+ */
+public class MailUtilsTest {
+
+	/**
+	 * Positive tests for checking if the provided string is valid
+	 */
+	@Test
+	public void isValidEmailTrue() {
+		Set<String> validEmails = new HashSet<>(Arrays.asList(
+				"a@b.com",
+				"a@bc.com",
+				"email@domain.com",
+				"john.doe@domain.com",
+				"john@ccs.neu.edu",
+				"john+spam@gmail.com",
+				"john@123.123.123.123",
+				"1234567890@mail.com",
+				"email@mail-dash.com",
+				"_______@underscore.com",
+				"email@domain.name",
+				"email@domain.co.in",
+				"john-doe@gmail.com"));
+		validEmails.forEach(e -> assertTrue(MailUtils.isValidEmail(e)));
+	}
+
+	/**
+	 * Negative tests for checking if the provided string is valid
+	 */
+	@Test
+	public void isValidEmailFalse() {
+		Set<String> invalidEmails = new HashSet<>(Arrays.asList(
+				null,
+				"",
+				"NoDomain",
+				"(#*&%^@mail.com",
+				"@mail.com",
+				"John Doe<johndoe@mail.com>",
+				"johnmail.com",
+				"a",
+				"a@b",
+				"ab@c"));
+		invalidEmails.forEach(e -> assertFalse(MailUtils.isValidEmail(e)));
+	}
+
+	/**
+	 * Test sending a mail with proper parameters
+	 * @throws IOException IOExceptions are thrown
+	 * @throws URISyntaxException URISyntaxException are thrown
+	 */
+	@Test
+	public void sendMailValidEmail() throws IOException, URISyntaxException {
+		String recipient = "shail@ccs.neu.edu";
+		String subject = "See me at my office";
+		String body = "Hello Shail,\n\nCome meet me at my office tomorrow at 1 PM.\n\nBest,\nMike\n";
+
+		MailUtils.sendMail(recipient, subject, body);
+	}
+
+	/**
+	 * Test sending a mail with improper recipient
+	 * @throws IOException IOExceptions are thrown
+	 * @throws URISyntaxException URISyntaxException are thrown
+	 */
+	@Test(expected = Exception.class)
+	public void sendMailInvalidEmail() throws IOException, URISyntaxException {
+		String recipient = "(#*&%^@mail.com";
+		String subject = "See me at my office";
+		String body = "Hello Shail,\n\nCome meet me at my office tomorrow at 1 PM.\n\nBest,\nMike\n";
+
+		MailUtils.sendMail(recipient, subject, body);
+	}
+}
