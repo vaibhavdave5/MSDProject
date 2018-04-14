f476e2e78d11c4fc4acfdad4c43c6612d7c28066
Vaibhav Shekhar Dave <vaibhavdave5@ccs.neu.edu>
2018-04-13 20:08:32 -0400

    Updated config

--- plagiarism-detector/src/main/java/utils/ConfigUtils.java
+++ plagiarism-detector/src/main/java/utils/ConfigUtils.java
@@ -6,10 +6,9 @@ import java.io.IOException;
 import java.util.Properties;
 import org.apache.log4j.Logger;
 
-
 /**
- * This class provides with the utilities to read and write the config 
- * files
+ * This class provides with the utilities to read and write the config files
+ * 
  * @author Vaibhav Dave
  * @since 4/9/2018
  *
@@ -18,36 +17,42 @@ public class ConfigUtils {
 
 	private static Logger logger = Logger.getLogger(ConfigUtils.class);
 	private static Properties properties = new Properties();
-	
+	private String propertiesLoc = "config.properties";
+
 	/**
-	 * This function helps the user to write a particular value
-	 * to the config.properties file
+	 * This function helps the user to write a particular value to the
+	 * config.properties file
+	 * 
 	 * @param String title
 	 * @param String value
 	 */
-	public void writeInConfig(String title, String value){
-	
-		properties.setProperty(title, value);
-		try {
-			properties.store(new FileOutputStream("config.properties",true), null);
-		} catch (IOException e) {
-			logger.error(e.toString());
+	public void writeInConfig(String title, String value) {
+
+		if (!value.equals(readConfig(title))) {
+			try {
+				properties.load(new FileInputStream(propertiesLoc));
+				properties.setProperty(title, value);
+				properties.store(new FileOutputStream(propertiesLoc), null);
+			} catch (IOException e) {
+				logger.error(e.toString());
+			}
 		}
 	}
-	
+
 	/**
-	 * This function helps the user to read a particular value
-	 * from the config.properties file
+	 * This function helps the user to read a particular value from the
+	 * config.properties file
+	 * 
 	 * @param String title
 	 * @param String value
 	 */
-	public String readConfig(String title){
+	public String readConfig(String title) {
 		try {
-			properties.load(new FileInputStream("config.properties"));
+			properties.load(new FileInputStream(propertiesLoc));
 		} catch (IOException e) {
 			logger.error(e.toString());
 		}
 		return properties.getProperty(title);
 	}
-	
+
 }
