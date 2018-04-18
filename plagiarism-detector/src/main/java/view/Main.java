1c289645ba373a42e5b903dc9c270bc33d4f37de
Samanjate Sood <samanjate@ccs.neu.edu>
2018-04-17 20:41:53 -0400

    starting app with swing

--- plagiarism-detector/src/main/java/view/Main.java
+++ plagiarism-detector/src/main/java/view/Main.java
@@ -3,11 +3,22 @@ package view;
 import controllers.ScreenController;
 import controllers.popups.PopupMessage;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.SceneBuilder;
+import javafx.scene.control.LabelBuilder;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.stage.StageBuilder;
+import javafx.stage.WindowEvent;
+
+import javax.swing.SwingUtilities;
+
 import org.apache.log4j.Logger;
 
 
@@ -42,7 +53,33 @@ public class Main extends Application {
 	}
 
 	public static void main(String[] args) {
-		launch(args);
+		SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new JFXPanel(); // this will prepare JavaFX toolkit and environment
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                    	try {
+                    		Stage primaryStage = new Stage();
+                			Parent root = FXMLLoader.load(getClass().getResource("/Start.fxml"));
+                			Scene scene = new Scene(root);
+                			scene.getStylesheets().add(getClass().getResource("/theme.css").toExternalForm());
+                			primaryStage.setResizable(false);
+                			primaryStage.getIcons().add(new Image("/images/app-icon.png"));
+                			ScreenController.setInstance(scene);
+                			primaryStage.setScene(scene);
+                			primaryStage.setTitle("Integrity Plagiarism Checker by Team-107");
+                			primaryStage.show();
+                		} catch(Exception e) {
+                	 		PopupMessage.getInstance().showError(null, null);
+                			logger.error(e);
+                		}
+                    }
+                });
+            }
+        });
+		//launch(args);
 	
 	}
 }
